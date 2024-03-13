e2d63663fd324e9306c11054382c4004b8a21cd7
Maxim Bondarev <m.bondarev@godeltech.com>
2024-03-13 15:14:16 +0100

    Remove unnecessary class Meteor

--- src/mappers/meteor.mapper.js
+++ src/mappers/meteor.mapper.js
@@ -1,22 +1,20 @@
-import { Meteor } from '../models/meteor.model.js';
-
 const extractedMeteors = (asteroidsData) => {
   return Object.entries(asteroidsData.near_earth_objects)
     .sort()
     .map((value) => value.splice(1, 1))
     .flat(2)
     .map((asteroid) => {
-      return new Meteor(
-        asteroid.id,
-        asteroid.name,
-        {
+      return {
+        id: asteroid.id,
+        name: asteroid.name,
+        diameter_meters: {
           estimated_diameter_min: asteroid.estimated_diameter.meters.estimated_diameter_min,
           estimated_diameter_max: asteroid.estimated_diameter.meters.estimated_diameter_max,
         },
-        asteroid.is_potentially_hazardous_asteroid,
-        asteroid.close_approach_data[0].close_approach_date_full,
-        asteroid.close_approach_data[0].relative_velocity.kilometers_per_second,
-      );
+        is_potentially_hazardous_asteroid: asteroid.is_potentially_hazardous_asteroid,
+        close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
+        relative_velocity_kps: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second,
+      };
     });
 };
 
