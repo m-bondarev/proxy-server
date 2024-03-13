a71bebb13e54c9b3cc6cb4f9a97500ceaf4f286d
Maxim Bondarev <m.bondarev@godeltech.com>
2024-03-13 15:15:23 +0100

    Remove unnecessary class Meteor

--- src/mappers/meteor.mapper.js
+++ src/mappers/meteor.mapper.js
@@ -13,7 +13,7 @@ const extractedMeteors = (asteroidsData) => {
         },
         is_potentially_hazardous_asteroid: asteroid.is_potentially_hazardous_asteroid,
         close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
-        relative_velocity_kps: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second,
+        relative_velocity_kps: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
       };
     });
 };
